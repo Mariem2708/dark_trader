f542b20cb58e7e76d56660923422f57dbf9a4a50
Mariem <Mariem2708@gmail.com>
2021-10-14 22:24:57 +0200

    scrapping

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,27 +1,43 @@
-require'nokogiri'
+require 'rubygems'
+require 'nokogiri'
 require 'open-uri'
 
-crypto = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
-
-names = []
-
-crypto.xpath('//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div').each do |node|
-names << node.text
-end 
-
-#puts names.inspect
 
 
-prices = []
-crypto.xpath('//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/div').each do |node|
-prices << node.text
+
+#cette methode vient récupérer le nom des cryptos
+def key
+ namekey = []
+ data_key = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+data_key.xpath('//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div').each do |node|
+	 namekey << node.text
+	end
+return namekey
 end
-#puts prices.inspect
+
+#cette methode viennt récupérer les valeurs des cryptos
+def value
+  namevalue = []
+  data_value = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+  data_value.xpath('//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/div').each do |node|
+      namevalue << node.text
+  	end
+  return namevalue
+end
+
+def zip(namekey,namevalue)
+array = []
+h = Hash[namekey.zip(namevalue)]
+array << h
+puts array.inspect
+end
 
 
-result = Hash[names.zip(prices)]
+#cette variable vient récupérer le nom et la valeur de la crypto
+def execute
+namekey = key
+namevalue = value
+h = zip(namekey,namevalue)
+end
 
-
-cryptomonnaies = []
-cryptomonnaies << result
-puts cryptomonnaies.inspect
+execute
